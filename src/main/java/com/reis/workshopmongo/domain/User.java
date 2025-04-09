44cc2a15ef1987ea460f9c330295a84b6fc91cf6
Matheus Reis <matheusreiscrs@gmail.com>
2025-04-09 20:19:03 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/reis/workshopmongo/domain/User.java
+++ src/main/java/com/reis/workshopmongo/domain/User.java
@@ -3,19 +3,24 @@ package com.reis.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection ="user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private String id;
-	private String nome;
+	private String name;
 	private String email;
 
 	public User() {
 	}
 
-	public User(String id, String nome, String email) {
+	public User(String id, String name, String email) {
 		this.id = id;
-		this.nome = nome;
+		this.name = name;
 		this.email = email;
 	}
 
@@ -27,12 +32,12 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.name = nome;
 	}
 
 	public String getEmail() {
